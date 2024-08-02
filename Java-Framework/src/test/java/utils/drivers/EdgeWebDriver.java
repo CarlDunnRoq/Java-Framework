bf55807fda4a824a401ceb72e861f3b1b8ced76f
Carl Dunn <carl.dunn@roq.co.uk>
2024-08-02 14:56:23 +0100

    Added the webdrivers for chrome, edge and firefox as well as the controller and settings

--- Java-Framework/src/test/java/utils/drivers/EdgeWebDriver.java
+++ Java-Framework/src/test/java/utils/drivers/EdgeWebDriver.java
@@ -1,4 +1,25 @@
 package utils.drivers;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 public class EdgeWebDriver {
+
+    private static WebDriver driver;
+
+    private static void setupEdgeDriver() {
+        WebDriverManager.edgedriver().setup();
+    }
+
+    public static WebDriver loadEdgeDriver(String edgeArgument) {
+        setupEdgeDriver();
+
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments(edgeArgument);
+
+        driver = new EdgeDriver(options);
+        return driver;
+    }
 }
